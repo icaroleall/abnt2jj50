e48f918bda8e01faedec9d90c8d7913ae1469dcb
Icaro Leal <icaroleal95@gmail.com>
2022-11-08 18:11:32 -0300

    feat: adding tap dance to accute and circumflex accents

--- keymaps/abnt2jj50/keymap.c
+++ keymaps/abnt2jj50/keymap.c
@@ -22,6 +22,8 @@ enum {
     TD_ALT_ALTGR,
     TD_WIN_PT_EN,
     TD_SMC_QUEST,
+    TD_AGUDO_CRASE,
+    TD_TIL_CIRCUNF,
 };
 
 enum layers {
@@ -44,6 +46,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_ALT_ALTGR] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
     [TD_WIN_PT_EN] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LGUI(KC_SPC)),
     [TD_SMC_QUEST] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, RALT(KC_W)),
+    [TD_AGUDO_CRASE]= ACTION_TAP_DANCE_DOUBLE(KC_LBRC, LSFT(KC_LBRC)),
+    [TD_TIL_CIRCUNF] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, LSFT(KC_QUOT)),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,15 +62,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |LS/Cap|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |En/Lsh|
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl |  F12 | Win  |Gr/Alt| Lower|    Space    | Raise|   /  | Left | Down |Right |
+     * | Ctrl | Win  |Gr/Alt|  F12 | Lower|    Space    | Raise|  / ? | Left | Down |Right |
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
-	KC_GRV, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, TD(TD_AGUDO_CRASE),
+	KC_GRV, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, TD(TD_TIL_CIRCUNF),
 	TD(TD_LSFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_SFTENT,
-	KC_LCTL, KC_F12, TD(TD_WIN_PT_EN), TD(TD_ALT_ALTGR), MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), TD(TD_SMC_QUEST), KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, TD(TD_WIN_PT_EN), TD(TD_ALT_ALTGR), KC_F12, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), TD(TD_SMC_QUEST), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 	
     /* LOWER
